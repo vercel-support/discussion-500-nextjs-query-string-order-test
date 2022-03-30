7e0579ad0c13c791fa26e99f24db8f5ee2772272
William Li <email@williamli.dev>
2022-03-30 16:21:26 +0800

    deploying api query string test

---
+++ pages/api/time.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler (req, res) {
+
+  const now = Date.now()
+  res.statusCode = 200
+  res.json({ now, query: req.query, url: req.url })
+}
